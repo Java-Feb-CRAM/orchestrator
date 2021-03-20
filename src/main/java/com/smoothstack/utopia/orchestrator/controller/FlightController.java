9c311755c642dd59c25c6820a066473a0c5e4f2b
Rob Maes <robert.maes@smoothstack.com>
2021-03-20 14:27:25 -0500

    ut-24 add CORS * for frontend testing

--- src/main/java/com/smoothstack/utopia/orchestrator/controller/FlightController.java
+++ src/main/java/com/smoothstack/utopia/orchestrator/controller/FlightController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
  * @author Rob Maes
  * Mar 18 2021
  */
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/flights")
 public class FlightController {
