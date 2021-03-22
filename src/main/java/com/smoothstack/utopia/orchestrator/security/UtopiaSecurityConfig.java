8708b4190a73a8174dd42f15b27d10d4b8c841c6
Craig Saunders <craig.saunders@smoothstack.com>
2021-03-22 19:10:51 -0400

    Remove super authentication manager as it's already a bean

--- src/main/java/com/smoothstack/utopia/orchestrator/security/UtopiaSecurityConfig.java
+++ src/main/java/com/smoothstack/utopia/orchestrator/security/UtopiaSecurityConfig.java
@@ -6,7 +6,6 @@ package com.smoothstack.utopia.orchestrator.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -39,13 +38,6 @@ public class UtopiaSecurityConfig extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception
-    {
-        return super.authenticationManagerBean();
     }
 
     @Bean
