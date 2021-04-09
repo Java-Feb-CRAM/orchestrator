c4f08e965b3c09dbc56dc7bd08b61f843ae5f4c2
Rob Maes <robert.maes@smoothstack.com>
2021-04-09 16:20:22 -0500

    change mysql env

--- src/main/java/com/smoothstack/utopia/orchestrator/controller/RegistrationController.java
+++ src/main/java/com/smoothstack/utopia/orchestrator/controller/RegistrationController.java
@@ -1,18 +1,16 @@
 /**
- * 
+ *
  */
 package com.smoothstack.utopia.orchestrator.controller;
 
+import com.smoothstack.utopia.orchestrator.util.ForwardUtil;
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import com.smoothstack.utopia.orchestrator.util.ForwardUtil;
 
 /**
  * @author Craig Saunders
@@ -21,37 +19,43 @@ import com.smoothstack.utopia.orchestrator.util.ForwardUtil;
 @CrossOrigin(origins = "*")
 @RestController
 public class RegistrationController {
-    @Autowired
-    RestTemplate restTemplate;
-    
-    private final String BASE_URI = "http://user-auth-service";
-    private final String NEW_USER = "/users/new";
-    private final String GENERATE_TOKEN = "/users/usernames/tokens/generate";
-    private final String ACTIVATE_USER = "/users/useranames/tokens/activate";
 
-    @PostMapping(path = NEW_USER)
-    public ResponseEntity<String> registerUserAccount(HttpServletRequest request)
-    {
-        return ForwardUtil.forwardRequest(
-            restTemplate, 
-            request, 
-            BASE_URI+NEW_USER);
-    }
+  @Autowired
+  RestTemplate restTemplate;
 
-    @PostMapping(path = GENERATE_TOKEN)
-    public ResponseEntity<String> generateVerificationToken(HttpServletRequest request) 
-    {
-        return ForwardUtil.forwardRequest(
-            restTemplate, 
-            request, 
-            BASE_URI+GENERATE_TOKEN);
-    }
+  private final String BASE_URI = "http://user-auth-service";
+  private final String NEW_USER = "/users/new";
+  private final String GENERATE_TOKEN = "/users/usernames/tokens/generate";
+  private final String ACTIVATE_USER = "/users/useranames/tokens/activate";
 
-    @PostMapping(path = ACTIVATE_USER)
-    public ResponseEntity<String> verifyEmail(HttpServletRequest request) {
-        return ForwardUtil.forwardRequest(
-            restTemplate, 
-            request, 
-            BASE_URI+ACTIVATE_USER);
-    }
+  @PostMapping(path = NEW_USER)
+  public ResponseEntity<String> registerUserAccount(
+    HttpServletRequest request
+  ) {
+    return ForwardUtil.forwardRequest(
+      restTemplate,
+      request,
+      BASE_URI + NEW_USER
+    );
+  }
+
+  @PostMapping(path = GENERATE_TOKEN)
+  public ResponseEntity<String> generateVerificationToken(
+    HttpServletRequest request
+  ) {
+    return ForwardUtil.forwardRequest(
+      restTemplate,
+      request,
+      BASE_URI + GENERATE_TOKEN
+    );
+  }
+
+  @PostMapping(path = ACTIVATE_USER)
+  public ResponseEntity<String> verifyEmail(HttpServletRequest request) {
+    return ForwardUtil.forwardRequest(
+      restTemplate,
+      request,
+      BASE_URI + ACTIVATE_USER
+    );
+  }
 }
