cfa8e9c274c24d0d58e13d06e319cfa9daedc956
Rob Maes <robert.maes@smoothstack.com>
2021-05-12 09:42:08 -0500

    UT-50 create post route for seat layout creation

--- src/main/java/com/smoothstack/utopia/orchestrator/controller/SeatLayoutController.java
+++ src/main/java/com/smoothstack/utopia/orchestrator/controller/SeatLayoutController.java
@@ -4,9 +4,11 @@ import com.smoothstack.utopia.orchestrator.util.ForwardUtil;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -43,5 +45,11 @@ public class SeatLayoutController {
       SEAT_LAYOUTS_ID_URL,
       seatLayoutId
     );
+  }
+
+  @Secured("ROLE_ADMIN")
+  @PostMapping
+  public ResponseEntity<String> createSeatLayout(HttpServletRequest request) {
+    return ForwardUtil.forwardRequest(restTemplate, request, SEAT_LAYOUTS_URL);
   }
 }
